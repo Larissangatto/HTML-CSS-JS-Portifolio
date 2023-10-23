5e2061088277e2814b073bb337ced53f97b40fec
Larissa Nathanna Gatto <larissangatto@gmail.com>
2023-10-23 10:19:03 -0300

    Portifólio em HTML CSS e JavaScript

---
+++ README.md
@@ -0,0 +1,3 @@
+# 💻Primeira Página em HTML CSS e JavaScript
+
+Projeto básico em html CSS e JavaScript.
---
+++ script.js
@@ -0,0 +1,11 @@
+// Navegando no menu
+function inicial() {
+  window.location.replace("inicial.html");
+}
+function projetos() {
+    window.location.replace("projetos.html");
+  }
+function portifolio() {
+  window.location.replace("portifolio.html");
+}
+
